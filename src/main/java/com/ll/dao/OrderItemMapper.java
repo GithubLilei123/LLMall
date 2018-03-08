7b2cbe91b93df0d8dc5d5fd802fcb3e158caacf5
GithubLilei123 <761527200@qq.com>
2018-03-08 19:15:03 +0800

    mybatis generator

---
+++ src/main/java/com/ll/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.ll.dao;
+
+import com.ll.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
---
+++ src/main/java/com/ll/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.ll.dao;
+
+import com.ll.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
